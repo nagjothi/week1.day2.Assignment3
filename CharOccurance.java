521c924229fcdf2c09d471f1c30a739353b70e1a
Nagajothi K <93859055+nagjothi@users.noreply.github.com>
2022-01-28 02:44:31 +0530

    Add files via upload

---
+++ CharOccurance.java
@@ -0,0 +1,22 @@
+package week1.day2.Assignment3;
+
+public class CharOccurance {
+	private static int i;
+
+	public static void main(String[] args) {
+		char name = 'n';
+		String str2 = "welcome to chennai";
+	    int count = 0;
+	    char[] str3 = str2 .toCharArray();
+	    for(int i = 0;i < str2.length();i++) {
+	    	if(name == str3[i]) {
+	    		count ++;
+	    	}
+	    }
+	    
+	    System.out.println("the character  "  + name +  "occurance is" +  count);
+	    
+	    		
+	}
+
+}
